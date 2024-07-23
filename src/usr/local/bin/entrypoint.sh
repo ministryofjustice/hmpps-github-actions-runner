e7057a1cd3062fac3d671f54b300ba02f0c48703
james Hart <james.hart@digital.justice.gov.uk>
2024-07-23 16:14:48 +0100

    another try to get diagnostics

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -41,20 +41,7 @@ fi
 echo "Checking the runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" --check --url "https://github.com/${GITHUB_REPOSITORY}" --pat ${GITHUB_TOKEN}
 
-echo
-echo
-echo "Internet check"
-echo "=============="
-echo
-cat "/actions-runner/_diag/InternetCheck*"
-echo
-echo
-echo "Actions check"
-echo "=============="
-echo
-cat "/actions-runner/_diag/ActionsCheck*"
-echo
-echo
+find "${ACTIONS_RUNNER_DIRECTORY}/_diag" | xargs cat
 
 echo "Configuring runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" ${EPHEMERAL_FLAG} \
