ca9a73a24727d745a20f45a8f6b1f7097ac6e6a3
james Hart <james.hart@digital.justice.gov.uk>
2024-07-23 16:02:49 +0100

    trying to check the configuration

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -38,6 +38,9 @@ else
   EPHEMERAL_FLAG=""
 fi
 
+echo "Checking the runner"
+bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" --check --url "https://github.com/${GITHUB_REPOSITORY}" --pat ${GITHUB_TOKEN}
+
 echo "Configuring runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" ${EPHEMERAL_FLAG} \
   --unattended \
