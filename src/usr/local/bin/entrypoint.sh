061dfcd9e2929d576f9282cf426a5ddecaa09dcb
james Hart <james.hart@digital.justice.gov.uk>
2024-07-23 16:46:22 +0100

    remove the noisy diagnostics

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -41,8 +41,6 @@ fi
 echo "Checking the runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" --check --url "https://github.com/${GITHUB_REPOSITORY}" --pat ${GITHUB_TOKEN}
 
-find "${ACTIONS_RUNNER_DIRECTORY}/_diag" | xargs cat
-
 echo "Configuring runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" ${EPHEMERAL_FLAG} \
   --unattended \
