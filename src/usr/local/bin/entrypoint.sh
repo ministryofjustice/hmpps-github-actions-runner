193376d786106f54afa0d6c349104bda16d2a1b2
james Hart <james.hart@digital.justice.gov.uk>
2024-11-28 12:45:22 +0000

    GH_AUTH_TOKEN added

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -17,7 +17,7 @@ getRegistrationToken=$(
     --location \
     --request "POST" \
     --header "X-GitHub-Api-Version: 2022-11-28" \
-    --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+    --header "Authorization: Bearer ${GH_AUTH_TOKEN}" \
     https://api.github.com/repos/"${GITHUB_REPOSITORY}"/actions/runners/registration-token | jq -r '.token'
 )
 export getRegistrationToken
