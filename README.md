56ddb59041bd5c2fc9bed8d4a623dfc5f04e3cfc
Matt <34448412+mattops@users.noreply.github.com>
2025-01-16 10:09:14 +0000

    Convert project to deploy org runners (#18)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # hmpps-github-actions-runner
-This deploys a self-hosted Github Runner to a single repository named in the repository variables.
+This deploys a self-hosted Github Runner to the github organisation.
 
 Documentation about it is [here](https://tech-docs.hmpps.service.justice.gov.uk/sreinternaldocs) 
 
@@ -22,5 +22,5 @@ It's deployed to Cloud Platforms, using Helm.
 ### Repo environment variables
 
 - GH_APP_ID - the corresponding AppId for the Github App
-- GH_REPOSITORY - the repo to which the runner should be registered
-- RUNNER_LABELS - the label by which the runner is invoked
+- RUNNER_LABEL - the label by which the runner is invoked
+- RUNNER_GROUP - the runner group to assign the new runners to.
--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -6,9 +6,10 @@ ACTIONS_RUNNER_DIRECTORY="/actions-runner"
 EPHEMERAL="${EPHEMERAL:-"false"}"
 
 echo "Runner parameters:"
-echo "  Repository: ${GITHUB_REPOSITORY}"
+echo "  GitHub org: ${GH_ORG}"
 echo "  Runner Name: $(hostname)"
-echo "  Runner Labels: ${RUNNER_LABELS}"
+echo "  Runner Labels: ${RUNNER_LABEL}"
+echo "  Runner group: ${RUNNER_GROUP}"
 
 echo "Obtaining registration token"
 getRegistrationToken=$(
@@ -16,9 +17,10 @@ getRegistrationToken=$(
     --silent \
     --location \
     --request "POST" \
+    --header "Accept: application/vnd.github+json" \
     --header "X-GitHub-Api-Version: 2022-11-28" \
     --header "Authorization: Bearer ${GH_AUTH_TOKEN}" \
-    https://api.github.com/repos/"${GITHUB_REPOSITORY}"/actions/runners/registration-token | jq -r '.token'
+    "https://api.github.com/orgs/${GH_ORG}/actions/runners/registration-token" | jq -r '.token'
 )
 export getRegistrationToken
 
@@ -39,16 +41,17 @@ else
 fi
 
 echo "Checking the runner"
-bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" --check --url "https://github.com/${GITHUB_REPOSITORY}" --pat ${GH_AUTH_TOKEN}
+bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" --check --url "https://github.com/${GH_ORG}" --pat ${GH_AUTH_TOKEN}
 
 echo "Configuring runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" ${EPHEMERAL_FLAG} \
   --unattended \
   --disableupdate \
-  --url "https://github.com/${GITHUB_REPOSITORY}" \
+  --url "https://github.com/${GH_ORG}" \
   --token "${REPO_TOKEN}" \
   --name "$(hostname)" \
-  --labels "${RUNNER_LABELS}"
+  --labels "${RUNNER_LABEL}" \
+  --runnergroup "${RUNNER_GROUP}"
 
 echo "Setting the 'ready' flag for Kubernetes liveness probe"
 touch /tmp/runner.ready
