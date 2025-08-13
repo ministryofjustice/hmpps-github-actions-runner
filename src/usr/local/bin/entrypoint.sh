703b7a8d1447c8a65be6069aa38f36e618dab811
james Hart <james.hart@digital.justice.gov.uk>
2025-08-13 16:24:33 +0100

    fix the invisible variable

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -5,9 +5,10 @@ set -euo pipefail
 echo "Setting up NVD vulnerability database mirror"
 echo "File location: /opt/vulnz/vulnz.jar"
 
-key_var="NVD_API_KEY_${POD_NUMBER}"
-export NVD_API_KEY="${!key_var}"
-echo "NVD API KEY (NVD_API_KEY_${POD_NUMBER}): ${NVD_API_KEY:0:3}...${NVD_API_KEY: -3}"
+echo "Pod number: ${POD_NUMBER}"
+# export key_var="NVD_API_KEY_${POD_NUMBER}"
+# export NVD_API_KEY="${!key_var}"
+# echo "NVD API KEY (NVD_API_KEY_${POD_NUMBER}): ${NVD_API_KEY:0:3}...${NVD_API_KEY: -3}"
 cd /opt/vulnz
 # ./vulnz.jar cve --cache --directory ./cache
 
