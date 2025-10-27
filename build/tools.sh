af69bc8d97d05fd577b9cfc39341e036e4264da7
s99mj <sandhya.gandalwar@justice.gov.uk>
2025-10-27 11:04:38 +0000

    Added azcli install

--- build/tools.sh
+++ build/tools.sh
@@ -25,6 +25,10 @@ function install_aws-cli() {
     || pip3 install --no-cache-dir awscli
 }
 
+function install_az-cli() {
+  pip3 install --no-cache-dir azure-cli
+}
+
 function install_git-lfs() {
   local DPKG_ARCH
   DPKG_ARCH="$(dpkg --print-architecture)"
